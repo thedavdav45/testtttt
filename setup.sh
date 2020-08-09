f9f47c84b9381854f9e2aedd67c8ccadc3ab205c
David <59321809+thedavdav45@users.noreply.github.com>
2020-08-09 15:15:31 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,9 @@
-echo Hello World
+echo "---------------------------------
+
+PyramidNode Setup Script
+Created by davdav45
+
+Version: 1.00
+
+---------------------------------
+Are you sure you would like to begin?"
